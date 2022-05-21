435df0e0e0e8e7f1189f051d944172c03a654194
asdqdsa <asdqdsa@protonmail.com>
2022-05-21 09:41:38 +0400

    recursion and usize

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
 fn main() {
-    println!("Fahrenheit -> Celsius");
+    println!("Fib?");
     let mut tempr = String::new();
     std::io::stdin()
         .read_line(&mut tempr)
         .expect("Error, can't read the input");
 
-    let tempr = tempr.trim().parse::<u32>()
+    let tempr = tempr.trim().parse::<u128>()
         .expect("Error!***#>");
     fib(tempr);
+    // println!("{}",fib(tempr));
+
 }
 
-fn fib(number: u32) {
+// fn fib(n :u32) -> u32 {
+//     // if n == 1 || n == 0 {
+//     //     return n;
+//     // } else {
+//     //     return fib(n - 2) + fib(n - 1)
+//     // }
 
-    // let mut iter :u32 = 1;
-    let mut prev_fib = 1;
-    let mut next_fib = prev_fib;
+//     match n {
+//         0 => n,
+//         1 => n,
+//         n => fib(n - 2) + fib(n - 1),
+//     }
+// }
+
+fn fib(mut number: u128) {
+    let mut prev_fib :u128 = 0;
+    let mut next_fib :u128 = 1;
 
     // loop fib
+    // let mut iter :u128 = 1;
     // loop {
     //     let mem_num = next_fib;
     //     next_fib = next_fib + prev_fib;
@@ -28,16 +43,21 @@ fn fib(number: u32) {
     //     }
     // }
 
-    // for fib
-    for _ in 0..number {
-        (prev_fib, next_fib) = (next_fib , next_fib + prev_fib);
+    // while fib
+    // let mut iter :u128 = 1;
+    while number > 0 {
+        // (prev_fib, next_fib) = (next_fib , next_fib + prev_fib);
+        let mem_num = next_fib;
+        next_fib = next_fib + prev_fib;
+        prev_fib = mem_num;
+        // iter += 1;
+        number -= 1;
     }
     println!("{}",next_fib);
 
-    // while fib
-    // while iter <= number {
-    // (prev_fib, next_fib) = (next_fib , next_fib + prev_fib);
-    // iter += 1;
+        // for fib
+    // for _ in 1..number {
+    //     (prev_fib, next_fib) = (next_fib , next_fib + prev_fib);
     // }
     // println!("{}",next_fib);
 }
